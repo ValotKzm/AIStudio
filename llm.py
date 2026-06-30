9dc5faced98664b2282d4dce353e08bc9d76950e
ValotKzm <darkvalot@gmail.com>
2026-06-30 17:41:39 +0200

    feat: managing LLM response errors

--- llm.py
+++ llm.py
@@ -4,18 +4,23 @@ from config import OPENAI_API_KEY, MODEL
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 def ask(system_prompt: str, user_prompt: str):
-    response = client.chat.completions.create(
-        model=MODEL,
-        messages=[
-            {
-                "role": "system",
-                "content": system_prompt
-            },
-            {
-                "role": "user",
-                "content": user_prompt
-            }
-        ]
-    )
+    try:
+        response = client.chat.completions.create(
+            model=MODEL,
+            messages=[
+                {
+                    "role": "system",
+                    "content": system_prompt
+                },
+                {
+                    "role": "user",
+                    "content": user_prompt
+                }
+            ]
+        )
+        
+        return response.choices[0].message.content
     
-    return response.choices[0].message.content
+    except Exception as e:
+        print(f"\n LLM error: {e}")
+        return "The AI could not generate a response."
