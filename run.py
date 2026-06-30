e040e186a94545beb9a777871420df8ab27a4c11
ValotKzm <darkvalot@gmail.com>
2026-06-30 17:23:54 +0200

    feat: add application entry point

---
+++ run.py
@@ -0,0 +1,21 @@
+from agents.developer import Developer
+from agents.manager import Manager
+from agents.tester import Tester
+
+developer = Developer()
+manager = Manager()
+tester = Tester()
+
+task = input("What would you like to do?")
+
+print("\n--- MANAGER ---\n")
+plan = manager.run(task)
+print(plan)
+
+print("\n--- DEVELOPER ---\n")
+code = developer.run(plan)
+print(code)
+
+print("\n--- TESTER ---\n")
+review = tester.run(code)
+print(review)
