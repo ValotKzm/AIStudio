e9906ce77308b10646c8d4408ddd00d42931869c
ValotKzm <darkvalot@gmail.com>
2026-07-01 00:29:35 +0200

    feat: add error handle and variable LLM & MODEL

--- config.py
+++ config.py
@@ -3,6 +3,18 @@ import os
 
 load_dotenv()
 
+# LLM configuration
+LLM_PROVIDER = os.getenv("LLM_PROVIDER")
+MODEL = os.getenv("MODEL")
+
+# OpenAI configuration
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-MODEL = "gpt-4.1-mini"
+
+# Configuration validation
+if LLM_PROVIDER is None:
+    raise ValueError("LLM_PROVIDER is missing from the .env file.")
+
+if MODEL is None:
+    raise ValueError("MODEL is missing from the .env file.")
+
