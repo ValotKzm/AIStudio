b251f62db698f2a3b278019b49ee0f91501c901e
ValotKzm <darkvalot@gmail.com>
2026-07-03 15:21:47 +0200

    feat: add initial python runner prototype

--- run.py
+++ run.py
@@ -2,12 +2,14 @@ from agents.developer import Developer
 from agents.manager import Manager
 from agents.tester import Tester
 from agents.filewriter import FileWriter
+from agents.python_runner import PythonRunner
 from workflow import Workflow
 
 workflow = Workflow([
     Manager(),
     Developer(),
     FileWriter(),
+    PythonRunner(),
     Tester(),
 ])
 
